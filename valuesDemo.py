b4f351975cf6ac921a6c92a300a664f3725450c9
LeoLau <liuyuansix@163.com>
2018-02-16 17:51:01 +0800

    添加了数字示例文件

--- valuesDemo.py
+++ valuesDemo.py
@@ -1,2 +1,32 @@
 message = "Hello Python world!"
 print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = 'i told my friend, "Python is my favorite language"'
+print(message)
+
+message = "the language 'Python' is named after Monty Python,not the snake."
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+print("Hello, " + full_name.title() +  "!")
+
+print("\tPython")
+
+
+space_string = ' python the space '
+print("-->"+space_string.rstrip()+"<--")#去掉末尾空白
+print("-->"+space_string.lstrip()+"<--")#去掉开头空白
+
+
